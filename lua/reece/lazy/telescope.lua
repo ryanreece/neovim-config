3eaa6f936051a04f36c7b1940cc93fffbefd932d
Ryan Reece <ryan@reece.ai>
2024-09-23 20:21:11 -0700

    feat: add telescope shortcuts for live_grep and buffers

--- lua/reece/lazy/telescope.lua
+++ lua/reece/lazy/telescope.lua
@@ -9,11 +9,10 @@ return {
         require('telescope').setup({})
 
         local builtin = require('telescope.builtin')
+        vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>ps', function()
-            builtin.grep_string({ search = vim.fn.input("grep > ") })
-        end)
+        vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
         vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
     end
 
