e05367f2abf8b4588861c20c80a4f36f46a13e59
Ryan Reece <ryan@reece.ai>
2024-10-05 22:03:38 -0700

    feat: auto wrap md on 79 chars & set 2 spaces for tf

--- lua/reece/autocmds.lua
+++ lua/reece/autocmds.lua
@@ -2,5 +2,23 @@ local autocmd = vim.api.nvim_create_autocmd
 
 autocmd({"BufRead", "BufNewFile"}, {
     pattern = "Jenkinsfile",
-    command = "set filetype=groovy",
+    callback = function ()
+        vim.bo.filetype = "groovy"
+    end,
+})
+
+autocmd("FileType", {
+    pattern = "markdown",
+    callback = function ()
+        vim.opt_local.textwidth = 79
+    end,
+})
+
+autocmd("FileType", {
+    pattern = "terraform",
+    callback = function ()
+        vim.opt_local.tabstop = 2
+        vim.opt_local.softtabstop = 2
+        vim.opt_local.shiftwidth = 2
+    end,
 })
