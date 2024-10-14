e1ad7927b589366dc53a9d3b327128e16214579d
Ryan Reece <ryan@reece.ai>
2024-10-13 20:23:38 -0700

    feat: install lsp for ruby and clean-up formatting

--- lua/reece/lazy/lsp.lua
+++ lua/reece/lazy/lsp.lua
@@ -1,20 +1,26 @@
 return {
     "neovim/nvim-lspconfig",
     dependencies = {
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-        "hrsh7th/nvim-cmp",
         "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-cmdline",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-path",
+        "hrsh7th/nvim-cmp",
         "j-hui/fidget.nvim",
+        "lukas-reineke/lsp-format.nvim",
+        "saadparwaiz1/cmp_luasnip",
+        "williamboman/mason-lspconfig.nvim",
+        "williamboman/mason.nvim",
     },
     config = function()
+        -- auto-complete
         local cmp = require('cmp')
         local cmp_lsp = require("cmp_nvim_lsp")
+        local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+        -- probably not needed...
+        -- see: https://www.reddit.com/r/neovim/comments/179syw2/reasons_to_call_vimlspprotocolmake_client/
         local capabilities = vim.tbl_deep_extend(
             "force",
             {},
@@ -22,7 +28,16 @@ return {
             cmp_lsp.default_capabilities()
         )
 
+        -- automatically format files on buffer write
+        require("lsp-format").setup {}
+        local on_attach = function(client, bufnr)
+            require("lsp-format").on_attach(client, bufnr)
+        end
+
+        -- unobtrusive lsp messages in the corner of the buffer
         require("fidget").setup({})
+
+        -- package manager for lsp servers
         require("mason").setup()
         require("mason-lspconfig").setup({
             ensure_installed = {
@@ -31,50 +46,47 @@ return {
                 'powershell_es',
                 'pylsp',
                 'rust_analyzer',
+                'standardrb',
                 'terraformls',
                 'jsonls',
                 'yamlls',
             },
             handlers = {
+                -- default handler for all lsp servers
+                -- e.g. this code will run automatically when detected file
+                -- servers are attached to the current buffer
                 function(server_name)
                     require("lspconfig")[server_name].setup {
-                        capabilities = capabilities
+                        capabilities = capabilities,
+                        on_attach = on_attach
                     }
                 end,
 
-                ["lua_ls"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.lua_ls.setup {
+                lua_ls = function()
+                    require("lspconfig").lua_ls.setup {
                         capabilities = capabilities,
+                        on_attach = on_attach,
                         settings = {
                             Lua = {
                                 runtime = { version = "Lua 5.4" },
                                 diagnostics = {
-                                    globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
+                                    globals = {
+                                        "bit",
+                                        "vim",
+                                        "it",
+                                        "describe",
+                                        "before_each",
+                                        "after_each"
+                                    },
                                 }
                             }
                         }
                     }
                 end,
-
-                pylsp = function()
-                    require('lspconfig').pylsp.setup {}
-                end,
-
-                terraformls = function()
-                    require('lspconfig').terraformls.setup {}
-                    vim.api.nvim_create_autocmd({ "BufWritePre" }, {
-                        pattern = { "*.tf", "*.tfvars" },
-                        callback = function()
-                            vim.lsp.buf.format()
-                        end,
-                    })
-                end,
             }
         })
 
-        local cmp_select = { behavior = cmp.SelectBehavior.Select }
-
+        -- configure how auto-complete works
         cmp.setup({
             snippet = {
                 expand = function(args)
@@ -95,6 +107,7 @@ return {
             })
         })
 
+        --- configure the diagnostic pop-ups for lsp
         vim.diagnostic.config({
             -- update_in_insert = true,
             float = {
